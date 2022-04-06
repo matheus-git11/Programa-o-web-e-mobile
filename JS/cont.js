e7a07d82cf061376f992109d6395a2720a1a6cf7
Matheus Albuquerque <matheus.albuquerque@hotmail.com>
2022-04-06 15:18:32 -0300

    starting JS

--- JS/cont.js
+++ JS/cont.js
@@ -1,14 +1,36 @@
  function criarDiv (){
-     const elem = document.createElement('div');
-     const phar = document.createElement('p');
-     const button = document.createElement('button');
     
-     
-     button.innerHTML="Limpar";
-     phar.innerHTML="Seu rotulo aqui";
-     elem.classList.add('contador');
+    if(document.querySelector('#texto').value===""){
+        alert("Insira um nome para seu contador");
 
-     document.querySelector('.field').appendChild(elem)
-     elem.appendChild(phar);
-     elem.appendChild(button);
+    }else{
+        const rotulo = document.querySelector('#texto').value;
+        const elem = document.createElement('div');
+        const phar = document.createElement('p');
+        const button = document.createElement('button');
+        
+       
+        
+        button.innerHTML="Limpar";
+        phar.innerHTML= rotulo;
+        elem.classList.add('contador');
+   
+        document.querySelector('.field').appendChild(elem);
+        elem.appendChild(phar);
+        elem.appendChild(button);
+
+    }
+     
+  
+    
+ }
+
+
+ function contador(){
+
+ }
+
+
+ function limpar(){
+
  }
